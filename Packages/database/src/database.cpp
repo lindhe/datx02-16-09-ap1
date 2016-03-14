e8b3e5fce1b75aa71ef8fc97a876b933e7745fd8
Axel Arkheden <axel.arkheden@gmail.com>
2016-03-14 15:09:49 +0100

    Adding updates to database. Function for updating the track pointers is now fully tested and working. Callback function for message recieving also added, not yet tested.

--- Packages/database/src/database.cpp
+++ Packages/database/src/database.cpp
@@ -4,7 +4,7 @@
 #include "std_msgs/Float64.h"
 #include "ackermann_msgs/AckermannDrive.h"
 //Comment out when gulliview_server is compiled
-//#include "gulliview_server/Pos.h"
+#include "gulliview_server/Pos.h"
 
 #include <iostream>
 #include <fstream>
@@ -44,17 +44,20 @@
     /**
     *  Index in array "track" to the 2 closest points to the car.
     */
-    int point1, point2, point1_distance, point2_distance;
+    int point1, point2;
     
-    //Coordinates for position and heading of car
-    int car_x, car_y, car_heading;
+    //Loop counter for callback
+    int callback_loop = 0;
+    
+    //Error saved for publishing.
+    float car_error = 0.0;
     
     //-------------------------------End---------------------------
     
     /**
     * Function that projects one vector onto another and returns the
     * resulting vector.
-    * Arguments: pointers to 2 vectors on the form; int[x,y].
+    * Arguments: pointers to 2 vectors on the form &int[x,y].
     * vec2 is projected on to vec1. res_vec is the vector variable
     * that should store the result, also on form int[x,y].
     */
@@ -68,34 +71,48 @@
         product2 = vec2[1] * vec1[1];
         numerator = product1 + product2;
         
-        cout << "Numerator: " << numerator << '\n';
-        
         product1 = vec1[0] * vec1[0];
         product2 = vec1[1] * vec1[1];
         denominator = product1 + product2;
-        
-        cout << "Denominator: " << denominator << '\n';
         
         numerator2 = (double)numerator;
         denominator2 = (double)denominator;
         
         result = numerator2 / denominator2;
         
-        cout << "Vector to scale: [" << vec1[0] << ","
-                << vec1[1] << "]" << '\n';
-        cout << "Result: " << result << '\n';
-        
         res_vec[0] = (double)vec1[0] * result;
         res_vec[1] = (double)vec1[1] * result;
-        
-        cout << "Projected vector: [" << res_vec[0] << ","
-                << res_vec[1] << "]" << '\n'; 
         
         return;
     }
     
     /**
-    * Arguments on the form int[x,y]. vec1 is the track vector,
+    * Function for calculating the distance between two points.
+    * Arguments on form &double[x,y].
+    */
+    //Tested and Working!
+    double distanceBetweenPoints(double* first_point, double* second_point){
+        double first_point_x, first_point_y, second_point_x, second_point_y;
+        double sum1, sum2, product1, product2, result;
+        
+        first_point_x = first_point[0];
+        first_point_y = first_point[1];
+        second_point_x = second_point[0];
+        second_point_y = second_point[1];
+        
+        sum1 = first_point_x - second_point_x;
+        sum2 = first_point_y - second_point_y;
+        
+        product1 = sum1 * sum1;
+        product2 = sum2 * sum2;
+        
+        result = sqrt(product1 + product2);
+        
+        return result;
+    }  
+    
+    /**
+    * Arguments on the form &int[x,y]. vec1 is the track vector,
     * and vec2 is the car vector. 
     * Function calculates the shortest distance from the car to
     * the track vector.
@@ -103,9 +120,8 @@
     * Returns the distance as a double;
     */
     
-    //Tested and working. Need to fix so the distance can be negative
-    //depending on the direction of the car. 
-    double calculateDistance(int* vec1, int* vec2){
+    //Tested and Working!
+    double calculateDistance(int* vec2, int* vec1){
         //Maybe add exceptions for accessing arrays
         double res_vec[2];
         
@@ -115,75 +131,93 @@
             return 0.0;
         }
         
-        if(vec2[0] == 0 && vec2[0] == 0){
-            return 0.0;
-        }
-        
         //Project Vector 2 onto Vector 1
         orthogonalProjection(&vec2[0], &vec1[0], &res_vec[0]);
-        cout << "Resulting vector: " << "[" << res_vec[0] << ","
-                << res_vec[1] << "]" << '\n';
         
         //Calculate distance
-        double sum1, sum2, result2, distance, product3, product4;
+        double result;
+        double car_vec[2];
+        car_vec[0] = (double)vec2[0];
+        car_vec[1] = (double)vec2[1];
+        //Here is this error, rethink.
+        result = distanceBetweenPoints(&car_vec[0], &res_vec[0]);
         
-        sum1 = vec2[0] - res_vec[0];
-        sum2 = vec2[1] - res_vec[1];
-        
-        product3 = sum1 * sum1;
-        product4 = sum2 * sum2;
-        
-        result2 = sqrt(product3 + product4);
-        
-        return result2;
+        return result;
     }
     
     /**
     * Function for updating pointers to the closest points on the track.
-    * Argument is on the form int[x,y,heading].
+    * Argument is on the form int[x,y].
     * Updates the current points to aim for on the track
     * - global variables point1 and point2 - if the current points are reached
     * or "unreachable".
     */
+    
+    //Tested and working!
     void updateIndicies(int* car_information){
         //Maybe add error handling if array is too small or too big.
-        int car_coordinate_x, car_coordinate_y, car_heading;
+        int car_coordinate_x, car_coordinate_y;
         int track_vector[2], car_vector[2];
-        double car_projection[2];
+        double car_projection[2], track_double[2];
         
         car_coordinate_x = car_information[0];
         car_coordinate_y = car_information[1];
-        car_heading = car_information[2];
         
         //Create vector from point 1 to point 2.
         track_vector[0] = track[point2][0] - track[point1][0];
         track_vector[1] = track[point2][1] - track[point1][1];
         
+        track_double[0] = (double)track_vector[0];
+        track_double[1] = (double)track_vector[1];
+                
         //Create vector from point 1 to position of car.
         car_vector[0] = car_coordinate_x - track[point1][0];
         car_vector[1] = car_coordinate_y - track[point1][1];
         
-        cout << "Track Vector: " << "[" << track_vector[0] << ","
-                << track_vector[1] << "]" << '\n';
-        cout << "Car Vector: " << "[" << car_vector[0] << ","
-                << car_vector[1] << "]" << '\n';
-        
-        orthogonalProjection(&car_vector[0], &track_vector[0],
-                &car_projection[0]);
-        
-        cout << "Car Projection Vector: " << "[" << car_projection[0] << ","
-                << car_projection[1] << "]" << '\n';
-        
-        double res;
-        res = calculateDistance(&track_vector[0], &car_vector[0]);
-        
-        cout << "Distance from car to track: " << res << '\n';
-        
         //Calculate orthogonal projection. If projected vector is
         //almost as long (marginal needs to be decided), the current points
         //are updated with the next points on the track.
+        orthogonalProjection(&car_vector[0], &track_vector[0],
+                                &car_projection[0]);                     
         
+        double lenght_of_track_vector;
+        double length_of_projection_vector;
+        double origo[2] = {0,0};
+        
+        lenght_of_track_vector = distanceBetweenPoints(&track_double[0],
+                                    &origo[0]);
+        
+        length_of_projection_vector = distanceBetweenPoints(&car_projection[0],
+                                        &origo[0]);
+        
+        //Change the value 1 to the margin we want to have to the next point.
+        //Checks if the projection of the car is almost at the right point,
+        //and if the projection vector is pointing in the same direction as
+        //the track vector.
+        
+        if((lenght_of_track_vector - length_of_projection_vector) <= 1 &&
+                ((track_double[0] * car_projection[0] > 0) ||
+                track_double[1] * car_projection[1] > 0)){
+            //Wrap around
+            if((track[point2 + 1][0] == 0 && track[point2 + 1][1] == 0) ||
+                point2 >= ((sizeof(track)/8) - 1)){
+                point1 = point2;
+                point2 = 0;
+            }
+            else if((track[point1 + 1][0] == 0 && track[point1 + 1][1] == 0) ||
+                point1 >= ((sizeof(track)/8) - 1)){
+                point1 = 0;
+                point2 = 1;
+            }
+            else {
+                point1 = point2;
+                point2 = point2 + 1;
+            }
+        return;
+        }
     }
+    
+    
     
     /**
     * Function for initializing track pointers
@@ -195,6 +229,7 @@
     //Tested and working.
     void initializeIndicies(int x1, int y1){
         double x2, y2, xdiff, ydiff, xdiff_pow, ydiff_pow, distance;
+        int point1_distance, point2_distance;
         point1 = 0;
         point2 = 0;
         point1_distance = numeric_limits<int>::max();
@@ -215,7 +250,6 @@
             ydiff_pow = ydiff * ydiff;
             
             distance = sqrt(xdiff_pow + ydiff_pow);
-            cout << "Distance between points: " << distance << '\n';
             
             if(distance < point1_distance){
                 point1_distance = (int)distance;
@@ -224,26 +258,16 @@
             }
             i++;
         }
-        int ape = 1;
-        cout << "Size of an int in memory: " << sizeof(ape) << '\n';
-        cout << "Size of array track: " << sizeof(track) << '\n';
-        cout << "Number of columns in array track: " << sizeof(track)/8 << '\n';
         
         //Wrap around
         if((track[point1 + 1][0] == 0 && track[point1 + 1][1] == 0) ||
                 point1 >= ((sizeof(track)/8) - 1)){
             
             point2 = 0;
-            cout << "Hello!" << '\n';
         }
         else{
             point2 = place + 1;
         }
-        cout << "Point1: " << "[" << track[point1][0] << ","
-                << track[point1][1] << "]" << '\n';
-        cout << "Point2: " << "[" << track[point2][0] << ","
-                << track[point2][1] << "]" << '\n';
-        
         return;
     }
     
@@ -267,10 +291,6 @@
                 //Get values from row
                 trackstream >> x >> y;
                 
-                //For test
-                cout << "X: " << x << '\n';
-                cout << "Y: " << y << '\n';
-                
                 track[i][0] = x;
                 track[i][1] = y;
                 
@@ -283,17 +303,68 @@
         return;
     }
 
-    void callback(const std_msgs::Float64::ConstPtr& msg){
-        ROS_INFO("I heard: [%.2f]", msg->data);
+   //Template to use the position from gulliview 
+   //Not yet tested.
+    void callback(const gulliview_server::Pos& msg){
+        ROS_INFO("I heard: x = %lld", (long long)msg.x);
+        ROS_INFO("I heard: y = %lld", (long long)msg.y);
+        ROS_INFO("I heard: heading = %lld", (long long)msg.heading);
+        int x, y, heading;
+        int car_coordinates[2], track_vector[2], car_vector[2];
+        double car_point[2], track_point[2], origo_point[2];
+        double distance_to_car, origo_to_car, origo_to_track;
+        float error;
+        x = (int)msg.x;
+        y = (int)msg.y;
+        heading = (int)msg.heading;
+        
+        //If it is the first message received, initialize pointers to the track.
+        if(callback_loop == 0){
+            initializeIndicies(x,y);
+        }
+        
+        //Update pointers to the track;
+        car_coordinates[0] = x;
+        car_coordinates[1] = y;
+        updateIndicies(&car_coordinates[0]);
+        
+        //Create a vector from closest point on the track to the car,
+        //and one from closest point on the track to the next point on
+        //the track.
+        car_vector[0] = x - track[point1][0];
+        car_vector[1] = y - track[point1][1];
+        
+        track_vector[0] = track[point2][0] - track[point1][0];
+        track_vector[1] = track[point2][1] - track[point1][0];
+        
+        //Calculate distance between the car and the track
+        distance_to_car = calculateDistance(&car_vector[0], &track_vector[0]);
+        
+        //Check which side of the circular track the car is on
+        car_point[0] = (double)x;
+        car_point[1] = (double)y;
+        
+        track_point[0] = (double)track[point1][0];
+        track_point[1] = (double)track[point1][1];
+        
+        origo_point[0] = 0;
+        origo_point[1] = 0;
+        
+        origo_to_car = distanceBetweenPoints(&car_point[0], &origo_point[0]);
+        origo_to_track = distanceBetweenPoints(&track_point[0],
+                            &origo_point[0]);
+        //Calculate if negative or positive distance
+        //Negative if right side of car, positive if left.
+        if(origo_to_car > origo_to_track){
+            distance_to_car = distance_to_car * -1;
+        }
+        
+        car_error = (float)distance_to_car;
+        
+        callback_loop ++;
     }
 
-/*    //Template to use the position from gulliview 
-    void callback(const gulliview_server::Pos& msg){
-        ROS_INFO("I heard: x = %lld", msg.x);
-        ROS_INFO("I heard: y = %lld", msg.y);
-        ROS_INFO("I heard: heading = %lld", msg.heading);
-    }
-*/
+
     int main(int argc, char **argv){
 
         /**
@@ -353,32 +424,19 @@
         */
         
         loadTrack();
-        
-        //Test new calculateDistance();
-        int car_testvector[3] = {5,-6,0};
-        
-        initializeIndicies(car_testvector[0], car_testvector[1]);
-        
-        updateIndicies(&car_testvector[0]);
-        
+
         int count = 0;
     
         while (ros::ok()){
-            if(count < 6) {
-                std_msgs::Float64 msg;
-                
-                float fl;
-                
-                //Steering Angle
-                fl = (float)track[count][1];
-                msg.data = fl;
-                
-                ROS_INFO("%.2f", msg.data);
-                
-                database_pub.publish(msg);
-                
-                usleep(3000000);
-            }
+            
+            //Create message to send to PID-controller
+            std_msgs::Float64 msg;
+            
+            msg.data = car_error;
+            
+            ROS_INFO("%.2f", msg.data);
+            
+            database_pub.publish(msg);
             
             ros::spinOnce();
             
