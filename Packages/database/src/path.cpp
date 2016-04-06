bb46bc5b903f3ac7112762e527d26d4475a335a3
Robert Gustafsson <robg@student.chalmers.se>
2016-04-06 08:57:31 +0200

    Update the circular path with another origo

--- Packages/database/src/path.cpp
+++ Packages/database/src/path.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 #define step 50
 #define radius 3000
+#define offset 1000
 int main () {
    ofstream file;
    file.open ("data.txt");
    for(int i= radius; i >= -radius; i = i - step){
-        file << i << " " << (int)sqrt(pow(radius,2) - pow(i, 2)) << endl;
+        file << i+offset << " " << (int)sqrt(pow(radius,2) - pow(i, 2))+offset << endl;
    } 
    for(int i= -radius+step; i < radius; i = i + step){
-        file << i << " " << (int)-sqrt(pow(radius,2) - pow(i, 2)) << endl;
+        file << i+offset << " " << (int)-sqrt(pow(radius,2) - pow(i, 2))+offset << endl;
    }
    file.close();
    return 0;
