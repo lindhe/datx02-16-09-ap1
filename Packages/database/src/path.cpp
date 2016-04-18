f8caa7119fa1add8a407ef9e49dd9e356a0e66dd
Robert Gustafsson <robg@student.chalmers.se>
2016-04-18 14:35:49 +0200

    Change the path with even distributed points

--- Packages/database/src/path.cpp
+++ Packages/database/src/path.cpp
@@ -2,17 +2,22 @@
 #include <fstream>
 #include <math.h>
 using namespace std;
-#define step 50
-#define radius 4000
-#define offset 1000
+#define step 100
+#define radius 3500
+#define offset 500
+#define limit 2*3.1415
 int main () {
    ofstream file;
    file.open ("data.txt");
-   for(int i= radius; i >= -radius; i = i - step){
+/*   for(int i= radius; i >= -radius; i = i - step){
         file << i+offset << " " << (int)sqrt(pow(radius,2) - pow(i, 2))+offset << endl;
    } 
    for(int i= -radius+step; i < radius; i = i + step){
         file << i+offset << " " << (int)-sqrt(pow(radius,2) - pow(i, 2))+offset << endl;
+   }*/
+   for(double i=0; i < limit; i += limit/step) {
+        cout << i << " " << limit << endl;
+        file << (int)(radius*cos(i)+offset) << " " << (int)(radius*sin(i)+offset) << endl;
    }
    file.close();
    return 0;
